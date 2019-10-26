0f9589288ac0c64ee81ebd92b9efa8572b83a6ec
Li Dong <dongli@lasg.iap.ac.cn>
2019-10-26 11:41:42 +0800

    Initialize nesting branch

---
+++ src/core/nest_mod.F90
@@ -0,0 +1,19 @@
+module nest_mod
+
+  use const_mod
+  use mesh_mod
+
+  implicit none
+
+  private
+
+  public nest_init
+
+contains
+
+  subroutine nest_init()
+
+
+  end subroutine nest_init
+
+end module nest_mod
