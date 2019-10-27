56f6b00d1f92b0e31f1b05e34700dd32ac0e87a3
Li Dong <dongli@lasg.iap.ac.cn>
2019-10-27 13:53:17 +0800

    Add more codes

--- src/core/nest_mod.F90
+++ src/core/nest_mod.F90
@@ -2,18 +2,64 @@ module nest_mod
 
   use const_mod
   use mesh_mod
+  use state_mod
 
   implicit none
 
   private
 
-  public nest_init
+  public nest_add_domain
+  public nest_exchange_one_way
+  public nest_exchange_two_way
+
+  type(mesh_type  ), allocatable, dimension(:  ) :: nested_mesh
+  type(static_type), allocatable, dimension(:  ) :: nested_static
+  type(state_type ), allocatable, dimension(:,:) :: nested_state
+  type(tend_type  ), allocatable, dimension(:,:) :: nested_tend
 
 contains
 
-  subroutine nest_init()
+  subroutine nest_add_domain(start_lon, end_lon, start_lat, end_lat, nest_ratio, root_mesh)
 
+    real(r8)       , intent(in) :: start_lon
+    real(r8)       , intent(in) :: end_lon
+    real(r8)       , intent(in) :: start_lat
+    real(r8)       , intent(in) :: end_lat
+    integer        , intent(in) :: nest_ratio
+    type(mesh_type), intent(in) :: root_mesh
 
-  end subroutine nest_init
+  end subroutine nest_add_domain
+
+  subroutine nest_init_domain(static, state)
+
+    type(static_type), intent(in) :: static
+    type(state_type) , intent(in) :: state
+
+    ! static -> nested_static
+    ! state -> nested_state
+
+  end subroutine nest_init_domain
+
+  subroutine nest_from_coarse_to_fine(root_mesh, root_state, nested_mesh, nested_state)
+
+    type(mesh_type) , intent(in   ) :: root_mesh
+    type(state_type), intent(in   ) :: root_state
+    type(mesh_type) , intent(inout) :: nested_mesh
+    type(state_type), intent(inout) :: nested_state
+
+    ! Interpolate from coarse grids to halo of fine grids.
+
+  end subroutine nest_from_coarse_to_fine
+
+  subroutine nest_from_fine_to_coarse(root_mesh, root_state, nested_mesh, nested_state)
+
+    type(mesh_type) , intent(inout) :: root_mesh
+    type(state_type), intent(inout) :: root_state
+    type(mesh_type) , intent(inout) :: nested_mesh
+    type(state_type), intent(inout) :: nested_state
+
+    ! Update coarse grids from fine grids.
+
+  end subroutine nest_from_fine_to_coarse
 
 end module nest_mod
