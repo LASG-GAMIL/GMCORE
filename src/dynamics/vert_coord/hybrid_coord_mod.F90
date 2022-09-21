7cddb50d0e97ade1215e63f68cf965e84b00857a
Li Dong <dongli@lasg.iap.ac.cn>
2022-09-21 18:04:25 +0800

    Add ecmwf_l90 hybrid params

--- src/dynamics/vert_coord/hybrid_coord_mod.F90
+++ src/dynamics/vert_coord/hybrid_coord_mod.F90
@@ -71,6 +71,8 @@ contains
         call hybrid_coord_test_l95(p0, ptop, hyai, hybi)
       case ('ecmwf_l50')
         call hybrid_coord_ecmwf_l50(p0, ptop, hyai, hybi)
+      case ('ecmwf_l90')
+        call hybrid_coord_ecmwf_l90(p0, ptop, hyai, hybi)
       case ('wrf_l16')
         call hybrid_coord_wrf_l16(p0, ptop, hyai, hybi)
         local_ptop = ptop
